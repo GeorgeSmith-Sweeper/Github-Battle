3bb2147fff236ab7142d0c912e255e8b764469eb
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-06-29 09:58:29 -0400

    badge implementation with Proptypes

--- app/index.js
+++ app/index.js
@@ -1,19 +1,37 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const PropTypes = require('prop-types');
+
 require('./index.css');
 
 
-class App extends React.Component {
+class Badge extends React.Component {
   render() {
     return (
       <div>
-        Hello React Training
+        <img
+          src={this.props.img}
+          alt='Avatar'
+          style={{ width: 100, height: 100 }}
+        />
+        <h1>Name: {this.props.name}</h1>
+        <h3>username: {this.props.username}</h3>
       </div>
     );
   }
 }
 
+Badge.propTypes = {
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+};
+
 ReactDOM.render(
-  <App />,
+  <Badge
+    name='Tyler McGinnis'
+    username='tylermcginnis'
+    img='https://avatars0.githubusercontent.com/u/2933439?v=3&s=460'
+  />,
   document.getElementById('app'),
 );
--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,10 @@ module.exports = {
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
     ],
   },
   plugins: [new HtmlWebpackPlugin({
     template: 'app/index.html',
-  })]
+  })],
 };
