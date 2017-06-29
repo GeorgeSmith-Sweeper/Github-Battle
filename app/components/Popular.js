74ca218e0b6b6422b228e02b89cf1e60638ceee6
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-06-29 16:48:17 -0400

    the language bar can now highlight

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,15 +1,31 @@
 import React from 'react';
 
 class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All',
+    };
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(lang) {
+    this.setState({ selectedLanguage: lang });
+  }
+
   render() {
     let languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
     return (
         <ul className="languages">
           {
-            languages.map((language) => {
+            languages.map((lang) => {
               return (
-                <li key={language}>
-                  {language}
+                <li
+                  style={lang === this.state.selectedLanguage ? { color: '#d0021b' } : null}
+                  key={lang}
+                  onClick={this.updateLanguage.bind(null, lang)}>
+                  {lang}
                 </li>
               );
             })
