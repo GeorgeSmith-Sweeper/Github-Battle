c49b896973e6465db1c2c4cdf0625f38966dc1ac
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-07-06 16:09:42 -0400

    styled ajax response into UI

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -22,11 +22,42 @@ function SelectLanguage(props) {
   );
 }
 
+function RepoGrid(props) {
+  return (
+    <ul className="popular-list">
+      {
+        props.repos.map((repo, index) => {
+          return (
+            <li key={repo.name} className="popular-item">
+              <div className="popular-rank">#{index + 1}</div>
+              <ul className="space-list-items">
+                <li>
+                  <img
+                    className="avatar"
+                    src={repo.owner.avatar_url}
+                    alt={`Avatar for ${repo.owner.login}`}
+                  />
+                </li>
+                <li><a href={repo.html_url}>{repo.name}</a></li>
+                <li>@{repo.owner.login}</li>
+                <li>{repo.stargazers_count} stars</li>
+              </ul>
+            </li>
+          );
+        })
+      }
+    </ul>
+  );
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired,
+};
+
 SelectLanguage.PropTypes = {
   selectedLanguage: PropTypes.string.isRequired,
   onSelect: PropTypes.func.isRequired,
 };
-
 
 
 class Popular extends React.Component {
@@ -40,12 +71,13 @@ class Popular extends React.Component {
   }
 
   componentDidMount() {
-    fetchPopularRepos(this.state.selectedLanguage)
-      .then(repo => console.log(repo));
+    this.updateLanguage(this.state.selectedLanguage);
   }
 
   updateLanguage(lang) {
-    this.setState({ selectedLanguage: lang });
+    this.setState({ selectedLanguage: lang, repos: null });
+    fetchPopularRepos(lang)
+      .then(repos => this.setState({ repos }));
   }
 
   render() {
@@ -55,6 +87,7 @@ class Popular extends React.Component {
           selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage}
         />
+        {!this.state.repos ? <p>LOADING</p> : <RepoGrid repos={this.state.repos} />}
       </div>
     );
   }
