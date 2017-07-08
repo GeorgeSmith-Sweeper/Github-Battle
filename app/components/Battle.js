db11f0593c4a867fc40ea0c1e9766a8331a2189a
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-07-08 11:18:48 -0400

    added handleSubmit for battle component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
 class Battle extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(id, username) {
+    this.setState(() => {
+      let newState = {};
+      newState[`${id}Name`] = username;
+      newState[`${id}Image`] = `https://github.com/${username}.png?size=200`;
+      return newState;
+    });
+  }
   render() {
     return (
       <div>
-        Battle
+        <div className="row">
+          ROW
+        </div>
       </div>
     );
   }
