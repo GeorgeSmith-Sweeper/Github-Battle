d85a2d620113aaeac2e21989fb3b85af4dae263f
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-07-26 18:07:56 -0400

    created PlayerPreview for reuse in Results and Battle component

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,14 +1,91 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import React from 'react';
+import queryString from 'query-string';
+import { battle } from '../utils/api';
+import PlayerPreview from './PlayerPreview';
+
+function Player(props) {
+  return (
+    <div>
+      <h1 className="header">{props.label}</h1>
+      <h3 style={{ textAlign: 'center' }}>Score: {props.score}</h3>
+    </div>
+  );
+}
+
+Player.propTypes = {
+  label: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  profile: PropTypes.object.isRequired,
+}
 
 class Results extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true,
+    };
+  }
+  componentDidMount() {
+    let players = queryString.parse(this.props.location.search);
+    battle([
+      players.playerOneName,
+      players.playerTwoName,
+    ]).then(results => {
+      if (results === null) {
+        return this.setState({
+          error: 'Check that both users exist on Github',
+          loading: false,
+        });
+      }
+      this.setState({
+        error: null,
+        winner: results[0],
+        loser: results[1],
+        loading: false,
+      });
+    });
+  }
   render() {
-    console.log(this.props)
+    const error = this.state.error;
+    const winner = this.state.winner;
+    const loser = this.state.loser;
+    const loading = this.state.loading;
+
+    if (loading) {
+      return <p>loading</p>
+    }
+
+    if (error) {
+      return (
+        <div>
+          <p>{error}</p>
+          <Link to="/battle">Reset</Link>
+        </div>
+      );
+    }
     return (
-      <div>
-        Results
+      <div className="row">
+        <Player
+          label="Winner"
+          score={winner.score}
+          profile={winner.profile}
+        />
+        <Player
+          label="Loser"
+          score={loser.score}
+          profile={loser.profile}
+        />
       </div>
     );
   }
 }
 
 export default Results;
+
+
+       // {JSON.stringify(this.state, null, 2)}
